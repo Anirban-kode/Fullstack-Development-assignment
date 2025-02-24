16ff5044781447ada603c8d1b2bcbeb5de1682e0
Anirban Mukherjee <anirbanmukherjee0987@gmail.com>
2025-02-24 22:47:12 +0530

    Add files via upload
    Third Assignment Question(App.js and UserCard.js)

--- week_3/App.js
+++ week_3/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import FormComponent from "./FormComponent"; // Import the form
+import UserCard from "./UserCard"; // Import the UserCard component
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Form Example</h1>
-      <FormComponent />
+    <div className="App p-4">
+      <h1 className="text-2xl font-bold text-center mb-4">User Cards</h1>
+      <UserCard name="John Doe" email="john.doe@example.com" />
+      <UserCard name="Jane Smith" email="jane.smith@example.com" />
     </div>
   );
 }
