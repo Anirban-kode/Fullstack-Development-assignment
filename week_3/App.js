1e6855675fe68f4a51a15c0788e5cb1eb5949312
Anirban Mukherjee <anirbanmukherjee0987@gmail.com>
2025-02-24 22:49:51 +0530

    Add files via upload

--- week_3/App.js
+++ week_3/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Button from "./Button"; // Import the Button component
+import LoginForm from "./LoginForm"; // Import the LoginForm component
 
 function App() {
   return (
-    <div className="App flex flex-col items-center p-4">
-      <h1 className="text-2xl font-bold mb-4">Tailwind Button</h1>
-      <Button text="Click Me" />
+    <div className="App">
+      <LoginForm />
     </div>
   );
 }
